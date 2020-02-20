da4254431f9d2ee81f4f0c532284355e7952c7ed
Liam Sutton <devliam90@gmail.com>
2020-02-20 17:42:37 +0000

    rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,35 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def rock_paper_scissors(n):
+    '''Returns all possbile plays of rps given inout n '''
+
+    # a place to store the plays
+    rps = ['rock', 'paper', 'scissors']
+    # a place to store the result
+    result = []
+
+    # define an inner recursive helper funtion
+    def recusive_helper(arr, n):
+        # base case if n is 0 return initial rps list
+        if n == 0:
+            return result.append(arr)
+        # else, loop over rps, feed arr to helper function
+        # and append all plays to result arr
+        for play in rps:
+            recusive_helper(arr + [play], n-1)
+
+    # initial call to kick off recursion
+    recusive_helper([], n)
+    return result
+
+
+print(rock_paper_scissors(4))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
